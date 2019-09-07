9f3bcd69570764107aba2511060203b7b3c5dcca
Erislandio <erislandio.soares@acct.global>
2019-09-07 11:07:35 -0300

    add login resolver

--- graphql/resolvers/user/authenticate.js
+++ graphql/resolvers/user/authenticate.js
@@ -1,4 +1,7 @@
 const User = require("../../../api/models/user");
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../../../config/auth.json')
 
 function generateToken(params = {}) {
   return jwt.sign(params, authConfig.secret, {
