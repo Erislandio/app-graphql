9a6a04746e3710cacca91354b8fe7d861b6e0781
Erislandio <erislandio.soares@acct.global>
2019-09-23 20:10:32 -0300

    add process env

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const graphqlHTTP = require("express-graphql");
 const schema = require("./graphql/schema");
 const root = require("./graphql/resolvers/index");
 require("./api/db/index");
-const CORS = require('cors')
+const CORS = require("cors");
 
 const app = express();
-app.use(CORS())
+app.use(CORS());
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -16,5 +16,5 @@ app.use(
   })
 );
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
 console.log("Running a GraphQL API server at http://localhost:4000/graphql");
